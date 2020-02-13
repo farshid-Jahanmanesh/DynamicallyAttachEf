a5ffd41d5111857f15abc58929657d7f20d1e1ba
farshid-Jahanmanesh <60971846+farshid-Jahanmanesh@users.noreply.github.com>
2020-02-13 11:57:05 +0330

    Create EntityTest.cs

---
+++ EntityTest.cs
@@ -0,0 +1,16 @@
+    
+    [DbSetAttach]
+    public class EntityTest
+    {
+
+    }
+    
+    public class EntityTestConfig:EntityTypeConfiguration<EntityTest>
+    {
+        
+        public EntityTestConfig()
+        {
+           //your fluent api code
+        }
+        
+    }
